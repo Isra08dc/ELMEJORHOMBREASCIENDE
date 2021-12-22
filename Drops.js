ea37076964d5240be00d468033170bdef187af6e
Isra08dc <90532141+Isra08dc@users.noreply.github.com>
2021-12-21 18:19:36 -0600

    Add files via upload

--- Drops.js
+++ Drops.js
@@ -2,7 +2,15 @@ class Umbrella {
     constructor(x,y){
         this.rain = Bodies.circle(x,xy,5,options)
     }
-upload(){
+createDrops(){
+    var dropPos=this.body.position;	
+		push()
+		translate(dropPos.x, dropPos.y);
+		fill(255,0,255)
+		imageMode(CENTER);
+		ellipseMode(CENTER);
+		image(this.image, 0,0,this.r*2, this.r*2)
+		pop()
     
 }
 
